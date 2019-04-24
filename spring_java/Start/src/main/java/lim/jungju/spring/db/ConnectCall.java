0bcc9f43ad3b69f2584c287011d981ec6d18485e
lim800 <lim800@gmail.com>
2019-04-24 10:27:02 +0900

    20190423

--- spring_java/Start/src/main/java/lim/jungju/spring/db/ConnectCall.java
+++ spring_java/Start/src/main/java/lim/jungju/spring/db/ConnectCall.java
@@ -7,12 +7,6 @@ public class ConnectCall {
 	public static void main(String[] args) {
 		ApplicationContext context = new GenericXmlApplicationContext("dbxml/conDB.xml");
 		ConncectDB conncectDB = (ConncectDB) context.getBean("conncectDB");
-		conncectDB.conn();
-		
-		
-				
-		
-		
-		
+		conncectDB.insert();
 	}
 }
